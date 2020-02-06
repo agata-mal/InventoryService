02b60567193f22787ed856988952020affada9c5
agata-mal <malinowska.agata1@gmail.com>
2020-02-06 18:33:04 +0100

    Dodanie jednostek do generowanego PDF

--- PDFServiceAPI/Enums/Unit.cs
+++ PDFServiceAPI/Enums/Unit.cs
@@ -1,8 +1,12 @@
-﻿namespace PDFServiceAPI.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PDFServiceAPI.Enums
 {
     public enum Unit
     {
+        [Display(Name = "szt")]
         Piece = 1,
+        [Display(Name = "kg")]
         Kilogram = 2
     }
 }
--- PDFServiceAPI/Service/PdfService.cs
+++ PDFServiceAPI/Service/PdfService.cs
@@ -1,4 +1,5 @@
-﻿using iText.Layout.Element;
+﻿using InventoryService.Enums;
+using iText.Layout.Element;
 using iText.Layout.Properties;
 using PDFServiceAPI.Models;
 using PDFServiceAPI.Service.Interfaces;
@@ -18,20 +19,21 @@ namespace PDFServiceAPI.Service
         }
         public Table AddTableToPdf(List<ItemModel> items)
         {
-            float[] columnWidth = { 50, 200, 100, 100, 100 };
+            float[] columnWidth = { 50, 100,100, 100, 100, 50 };
             Table tableLayout = new Table(columnWidth);
             tableLayout.SetTextAlignment(TextAlignment.CENTER);
             tableLayout.AddHeaderCell("kod produktu");
             tableLayout.AddHeaderCell("nazwa produktu");
+            tableLayout.AddHeaderCell("Jm");
             tableLayout.AddHeaderCell("oczekiwana ilosc");
             tableLayout.AddHeaderCell("rzeczywista ilosc");
             tableLayout.AddHeaderCell("roznica");
 
             foreach (var item in items)
             {
-
                 tableLayout.AddCell(item.ItemNumber.ToString());
                 tableLayout.AddCell(item.ItemName);
+                tableLayout.AddCell(item.Unit.GetDisplayName());
                 tableLayout.AddCell(item.ExpectedAmount.ToString());
                 tableLayout.AddCell(item.RealAmount.ToString());
                 tableLayout.AddCell(GetDifferenceCell(item.RealAmount, item.ExpectedAmount));
