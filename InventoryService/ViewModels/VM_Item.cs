7c4177d63e739f83e76e710e7850f6a9d089ac89
agata-mal <malinowska.agata1@gmail.com>
2020-02-10 12:17:44 +0100

    dodanie Range w VM_Item

--- InventoryService/ViewModels/VM_Item.cs
+++ InventoryService/ViewModels/VM_Item.cs
@@ -1,10 +1,12 @@
 ﻿using InventoryService.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryService.ViewModels
 {
     public class VM_Item
     {
         public int Id { get; set; }
+        [Range(1,9999)]
         public int ItemNumber { get; set; }
         public string ItemName { get; set; }
         public Unit Unit { get; set; }
